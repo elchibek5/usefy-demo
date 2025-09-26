dddb45fc78ac9b2c51283ab377ed2f7952f0d779
elchibekdastanov <elchibekdastanov@gmail.com>
2025-09-26 17:06:52 -0500

    Moving controllers into the correct packages

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,10 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
 
 @RestController
 class HelloRestController {
