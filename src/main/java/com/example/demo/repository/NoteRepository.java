f8702054ce22e7a87325a106250cbb8d3d8e39fa
elchibekdastanov <elchibekdastanov@gmail.com>
2025-09-27 22:34:30 -0500

    Changed files to proper folders

--- src/main/java/com/example/demo/repository/NoteRepository.java
+++ src/main/java/com/example/demo/repository/NoteRepository.java
@@ -1,6 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Note;
+import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoteRepository extends JpaRepository<Note, Long> {}
+public interface NoteRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
