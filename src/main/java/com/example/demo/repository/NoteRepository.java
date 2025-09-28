54c92de996a5d50998fdd4ca68e4ca7a863b2db7
elchibekdastanov <elchibekdastanov@gmail.com>
2025-09-27 23:25:49 -0500

    imported Note

--- src/main/java/com/example/demo/repository/NoteRepository.java
+++ src/main/java/com/example/demo/repository/NoteRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.Note;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NoteRepository extends JpaRepository<User, Long> {
+public interface NoteRepository extends JpaRepository<Note, Long> {
     User findByUsername(String username);
 }
