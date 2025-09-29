4a7f4273c5ce5a34c015cb588356b869f73d6b63
elchibekdastanov <elchibekdastanov@gmail.com>
2025-09-28 23:30:43 -0500

    now can find by user id

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +16,7 @@ public class UserService {
     }
 
     public List<User> findAll() { return repo.findAll(); }
+    public Optional<User> findById(Long id) { return repo.findById(id); }
     public User save(User u) { return repo.save(u); }
     public User findByUsername(String username) { return repo.findByUsername(username); }
 }
