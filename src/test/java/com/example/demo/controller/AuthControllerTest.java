d4399a754c4febf9f0d357fcc7cc47d0890da7da
elchibekdastanov <elchibekdastanov@gmail.com>
2025-11-07 03:10:11 -0600

    Changed the test cases to make it work properly

--- src/test/java/com/example/demo/controller/AuthControllerTest.java
+++ src/test/java/com/example/demo/controller/AuthControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
