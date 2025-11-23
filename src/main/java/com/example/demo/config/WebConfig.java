1dd598af4bb69f3dce34073038fb59bfd50e771b
elchibekdastanov <elchibekdastanov@gmail.com>
2025-11-22 23:26:17 -0600

    Enabled React to run

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -21,4 +21,13 @@ public class WebConfig {
             }
         };
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
