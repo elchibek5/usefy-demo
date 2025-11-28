721328fbf1c5eae2e1b87196bc053ed723413dbd
elchibekdastanov <elchibekdastanov@gmail.com>
2025-11-27 23:12:11 -0600

    .

--- src/main/java/com/example/demo/service/ChatService.java
+++ src/main/java/com/example/demo/service/ChatService.java
@@ -2,15 +2,9 @@ package com.example.demo.service;
 
 import com.example.demo.dto.ChatRequestDto;
 import com.example.demo.dto.ChatResponseDto;
-import org.springframework.stereotype.Service;
 
-@Service
-public class ChatService {
+public interface ChatService {
 
-    public ChatResponseDto getChatResponse(ChatRequestDto request) {
-        ChatResponseDto dto = new ChatResponseDto();
-        dto.setReply("AI says: " + request.getMessage());
-        return dto;
-    }
+    ChatResponseDto getChatResponse(ChatRequestDto requestDto);
+
 }
-
